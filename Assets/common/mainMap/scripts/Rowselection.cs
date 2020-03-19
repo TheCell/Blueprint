f30bc7c328b7ef0d6ace5eef7f843a3cc6f6d9ac
TheCell <simon_hischier@hotmail.com>
2020-03-19 23:17:35 +0100

    moved scripts, added started select script

---
+++ Assets/common/mainMap/scripts/Rowselection.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rowselection : MonoBehaviour
+{
+    private void Start()
+    {
+		PushAllObjects();
+	}
+
+    private void Update()
+    {
+        
+    }
+
+	private void PushAllObjects()
+	{
+		RaycastHit[] rayHits;
+		Vector3 rayStart = transform.position;
+		rayStart.y += 1; // set to middle of the object height
+
+		Ray ray = new Ray(rayStart, -transform.right);
+		rayHits = Physics.RaycastAll(ray);
+		for (int i = 0; i < rayHits.Length; i++)
+		{
+			UnitBasics unitBasics = rayHits[i].collider.GetComponent<UnitBasics>();
+			if (unitBasics != null)
+			{
+				unitBasics.MoveToDirection(Direction.South);
+			}
+		}
+	}
+}
---
+++ Assets/common/mainMap/scripts/UnitBasics.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitBasics : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+	public void MoveToDirection(Direction moveTo)
+	{
+		RaycastHit rayhit;
+		Vector3 newposition = Vector3.zero;
+
+		Vector3 objectMiddle = transform.position;
+		objectMiddle.y += 1; // object middle
+		if (Physics.Raycast(objectMiddle, Vector3.down, out rayhit))
+		{
+			FloorTile floorTile = rayhit.collider.GetComponent<FloorTile>();
+			if (floorTile != null)
+			{
+				newposition = floorTile.GetNextPosition(moveTo);
+				Debug.Log(newposition);
+			}
+		}
+		else
+		{
+			Debug.LogError("no floor tile");
+		}
+
+		transform.position = newposition;
+	}
+}
