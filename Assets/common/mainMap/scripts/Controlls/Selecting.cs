3292d45a3e6527e59c059812754523491a2ed68c
TheCell <simon_hischier@hotmail.com>
2020-03-29 15:45:06 +0200

    playerinput basics working

---
+++ Assets/common/mainMap/scripts/Controlls/Selecting.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selecting : MonoBehaviour
+{
+	private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+	private void OnMove()
+	{
+		Debug.Log(gameObject.name + ": I'm moving");
+	}
+}
