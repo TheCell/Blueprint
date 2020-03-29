a87ac563a31430ec54c1f5ac13c18661c2a4251f
Valentin Berger <V4L3@users.noreply.github.com>
2020-03-29 19:13:07 +0200

    [Not Working as inteded] move selected field with keyboard input

--- Assets/common/mainMap/scripts/FloorTileScripts/FloorTile.cs
+++ Assets/common/mainMap/scripts/FloorTileScripts/FloorTile.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class FloorTile : MonoBehaviour
 {
     private Vector3[] surroundingPositions = new Vector3[4];
+    private FloorTile_UnitPlacing[] surroundingTiles = new FloorTile_UnitPlacing[4];
+
 
 
     private void Start()
@@ -16,87 +18,102 @@ public class FloorTile : MonoBehaviour
         if(Physics.Raycast(vecOrigin, Vector3.forward, out hit, 2.0f))
         {
             surroundingPositions[0] = hit.collider.gameObject.transform.position;
+            surroundingTiles[0] = hit.collider.gameObject.GetComponent<FloorTile_UnitPlacing>();
         }
         else
         {
             hits = Physics.RaycastAll(vecOrigin, Vector3.back);
             Vector3 lastElem = transform.position;
+            FloorTile_UnitPlacing lastTile = this.GetComponent<FloorTile_UnitPlacing>();
 
             for(int i = 0; i < hits.Length; i++)
             {
                 if(lastElem.z > hits[i].collider.gameObject.transform.position.z)
                 {
                     lastElem.z = hits[i].collider.gameObject.transform.position.z;
+                    lastTile = hits[i].collider.gameObject.GetComponent<FloorTile_UnitPlacing>();
                 }
             }
 
             surroundingPositions[0] = lastElem;
-
+            surroundingTiles[0] = lastTile;
 
         }
 
         if (Physics.Raycast(vecOrigin, Vector3.right, out hit, 2.0f))
         {
             surroundingPositions[1] = hit.collider.gameObject.transform.position;
+            surroundingTiles[1] = hit.collider.gameObject.GetComponent<FloorTile_UnitPlacing>();
         }
         else
         {
             hits = Physics.RaycastAll(vecOrigin, Vector3.left);
             Vector3 lastElem = transform.position;
+            FloorTile_UnitPlacing lastTile = this.GetComponent<FloorTile_UnitPlacing>();
+
 
             for (int i = 0; i < hits.Length; i++)
             {
                 if (lastElem.x > hits[i].collider.gameObject.transform.position.x)
                 {
                     lastElem.x = hits[i].collider.gameObject.transform.position.x;
+                    lastTile = hits[i].collider.gameObject.GetComponent<FloorTile_UnitPlacing>();
                 }
             }
 
             surroundingPositions[1] = lastElem;
-
+            surroundingTiles[1] = lastTile;
         }
 
         if (Physics.Raycast(vecOrigin, Vector3.back, out hit, 2.0f))
         {
             surroundingPositions[2] = hit.collider.gameObject.transform.position;
+            surroundingTiles[2] = hit.collider.gameObject.GetComponent<FloorTile_UnitPlacing>();
         }
         else
         {
             hits = Physics.RaycastAll(vecOrigin, Vector3.forward);
             Vector3 lastElem = transform.position;
+            FloorTile_UnitPlacing lastTile = this.GetComponent<FloorTile_UnitPlacing>();
 
             for (int i = 0; i < hits.Length; i++)
             {
                 if (lastElem.z < hits[i].collider.gameObject.transform.position.z)
                 {
                     lastElem.z = hits[i].collider.gameObject.transform.position.z;
+                    lastTile = hits[i].collider.gameObject.GetComponent<FloorTile_UnitPlacing>();
                 }
             }
 
             surroundingPositions[2] = lastElem;
+            surroundingTiles[2] = lastTile;
 
         }
 
         if (Physics.Raycast(vecOrigin, Vector3.left, out hit, 2.0f))
         {
             surroundingPositions[3] = hit.collider.gameObject.transform.position;
+            surroundingTiles[3] = hit.collider.gameObject.GetComponent<FloorTile_UnitPlacing>();
         }
         else
         {
             hits = Physics.RaycastAll(vecOrigin, Vector3.right);
             Vector3 lastElem = transform.position;
+            FloorTile_UnitPlacing lastTile = this.GetComponent<FloorTile_UnitPlacing>();
 
             for (int i = 0; i < hits.Length; i++)
             {
                 if (lastElem.x < hits[i].collider.gameObject.transform.position.x)
                 {
                     lastElem.x = hits[i].collider.gameObject.transform.position.x;
+                    lastTile = hits[i].collider.gameObject.GetComponent<FloorTile_UnitPlacing>();
                 }
             }
 
             surroundingPositions[3] = lastElem;
-		}
-	}
+            surroundingTiles[3] = lastTile;
+        }
+    }
 
     public Vector3 GetNextPosition(Direction direction)
     {
@@ -112,6 +129,23 @@ public class FloorTile : MonoBehaviour
                 return surroundingPositions[3];
             default:
                 return Vector3.zero;
+        }
+    }
+
+    public FloorTile_UnitPlacing GetNextPosition(Direction direction, bool returnObject)
+    {
+        switch (direction)
+        {
+            case Direction.North:
+                return surroundingTiles[0];
+            case Direction.East:
+                return surroundingTiles[1];
+            case Direction.South:
+                return surroundingTiles[2];
+            case Direction.West:
+                return surroundingTiles[3];
+            default:
+                return null;
         }
     }
 }
--- Assets/common/mainMap/scripts/FloorTileScripts/FloorTile_UnitPlacing.cs
+++ Assets/common/mainMap/scripts/FloorTileScripts/FloorTile_UnitPlacing.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
+[RequireComponent(typeof(FloorTile))]
 public class FloorTile_UnitPlacing : MonoBehaviour
 {
     SelectionHighlight selection;
-    private bool isHovered, isFull = false;
+    [SerializeField] private Direction moveDirection;
+
+    private bool isSelected, isFull = false;
+    FloorTile floorTile;
 
     public Transform prefab;
 
@@ -13,6 +17,7 @@ public class FloorTile_UnitPlacing : MonoBehaviour
     private void Start()
     {
         selection = gameObject.GetComponent<SelectionHighlight>();
+        floorTile = gameObject.GetComponent<FloorTile>();
     }
 
     private void OnMouseEnter()
@@ -21,7 +26,7 @@ public class FloorTile_UnitPlacing : MonoBehaviour
         {
             selection.SetHighlight(true);
         }
-        isHovered = true;
+        isSelected = true;
     }
 
     private void OnMouseExit()
@@ -30,17 +35,81 @@ public class FloorTile_UnitPlacing : MonoBehaviour
         {
             selection.SetHighlight(false);
         }
-        isHovered = false;
+        isSelected = false;
     }
 
+    private void OnMove(InputValue inputValue)
+    {
+        //Debug.Log("FloorTile" + inputValue.Get<Vector2>());
+        Vector2 dir = inputValue.Get<Vector2>();
+        FloorTile_UnitPlacing next;
+
+
+        if (isSelected)
+        {
+            if (dir.x <= -0.1)
+            {
+                Debug.Log("WEST");
+                moveDirection = Direction.West;
+                next = floorTile.GetNextPosition(moveDirection,true);
+                next.SetHighlight(true);
+                next.isSelected = true;
+                SetHighlight(false);
+                isSelected = false;
+            }
+            else if (dir.x >= 0.1)
+            {
+                Debug.Log("EAST");
+                moveDirection = Direction.East;
+                next = floorTile.GetNextPosition(moveDirection, true);
+                next.SetHighlight(true);
+                next.isSelected = true;
+                SetHighlight(false);
+                isSelected = false;
+
+            }
+            else if (dir.y <= -0.1)
+            {
+                Debug.Log("SOUTH");
+                moveDirection = Direction.South;
+                next = floorTile.GetNextPosition(moveDirection, true);
+                next.SetHighlight(true);
+                next.isSelected = true;
+                SetHighlight(false);
+                isSelected = false;
+
+            }
+            else if (dir.y >= 0.1)
+            {
+                Debug.Log("NORTH");
+                moveDirection = Direction.North;
+                next = floorTile.GetNextPosition(moveDirection, true);
+                next.SetHighlight(true);
+                next.isSelected = true;
+                SetHighlight(false);
+                isSelected = false;
+
+            }
+            else
+            {
+                Debug.Log("Threshold not met");
+                //next = this;
+            }
+        }
+    }
 
     private void OnFire()
     {
-        if (isHovered && !isFull)
+        if (isSelected && !isFull)
         {
             Vector3 spawnPosition = gameObject.transform.position;
             Instantiate(prefab, spawnPosition, Quaternion.Euler(-90, 0, 0));
             isFull = true;
         }
+    }
+
+    public void SetHighlight(bool highlight)
+    {
+        selection.SetHighlight(highlight);
     }
 }
