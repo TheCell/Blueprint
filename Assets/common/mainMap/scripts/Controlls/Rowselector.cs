2a03c1065fe21e05f92c67c3ecd18d560bbc174d
TheCell <simon_hischier@hotmail.com>
2020-03-29 16:30:00 +0200

    added all methods, renamed class

---
+++ Assets/common/mainMap/scripts/Controlls/Rowselector.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Rowselector : MonoBehaviour
+{
+	private PlayerInputManager playerInputManager;
+
+	private void Start()
+    {
+		playerInputManager = GameObject.FindObjectOfType<PlayerInputManager>();
+		if (playerInputManager != null)
+		{
+			//Debug.Log(playerInputManager.name + " " + playerInputManager.playerCount);
+		}
+    }
+
+    private void Update()
+    {
+
+	}
+
+	private void OnMove()
+	{
+		Debug.Log(gameObject.name + ": I'm OnMove");
+	}
+
+	private void OnFire()
+	{
+		Debug.Log(gameObject.name + ": I'm OnFire");
+	}
+
+	private void OnLook()
+	{
+		Debug.Log(gameObject.name + ": I'm OnLook");
+	}
+}
