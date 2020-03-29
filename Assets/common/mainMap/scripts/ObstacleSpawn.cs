c9fe9089fa066e7368f077ad04fda037a153d418
Valentin Berger <V4L3@users.noreply.github.com>
2020-03-29 14:38:12 +0200

    Random Obstacle Spawn function (spawns a number of unit_wall objects depending on the size of the playing field)

---
+++ Assets/common/mainMap/scripts/ObstacleSpawn.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawn : MonoBehaviour
+{
+    //List<GameObject> floorTiles = new List<GameObject>();
+    private int nrOfObstacles;
+
+    public Transform prefab;
+
+    void Start()
+    {
+        List<GameObject> floorTiles = new List<GameObject>(GameObject.FindGameObjectsWithTag("floor_tile"));
+        nrOfObstacles = (int) Mathf.Sqrt(floorTiles.Count);
+
+        for(int i = 1; i <= nrOfObstacles; i++)
+        {
+            int randomIndex = Random.Range(0, floorTiles.Count);
+            Vector3 spawnPosition = floorTiles[randomIndex].transform.position;
+            Instantiate(prefab, spawnPosition, Quaternion.Euler(-90, 0, 0));
+            floorTiles.RemoveAt(randomIndex);
+            Debug.Log(floorTiles.Count);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
